52adbcde24236cc89051e028d71ee8b24e19ff84
ehsan2793 <ehsan2793@gmail.com>
2021-10-08 00:54:27 -0700

    all task can be viewed

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,28 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig')
+
+const getAll = async () => {
+    const tasks = await db('tasks as t')
+        .leftJoin("projects as p", 'p.project_id', 't.project_id')
+        .select('t.*', 'p.project_name', 'project_description')
+    return tasks
+}
+
+const getById = async (id) => {
+    const project = await db('tasks')
+        .where({ project_id: id }).first();
+    return project;
+};
+
+const insert = async (task) => {
+    const [id] = await db('tasks').insert(task)
+    return getById(id)
+}
+
+
+
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+}
