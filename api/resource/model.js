ba43e81430e5a67fa3c88bb3a14d6847ce8f3be7
ehsan2793 <ehsan2793@gmail.com>
2021-10-07 22:09:44 -0700

    we can post new resorces

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,2 +1,29 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
 
+const getAll = async () => {
+    const allResources = await db('resources')
+    return allResources
+}
+
+const getById = async (id) => {
+    const resource = await db('resources').where({ resource_id: id }).first();
+    return resource;
+};
+
+const insert = async (resources) => {
+    const [id] = await db('resources').insert(resources)
+    return getById(id)
+}
+
+
+// insert into resources
+// (resource_name,resource_description)
+// values 
+// ('pen','pen is something that you can write with')
+
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,8 +1,17 @@
 // build your `/api/resources` router here
+const rm = require('./model')
+
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json("hello form resources")
+router.get('/', async (req, res) => {
+    const allResources = await rm.getAll()
+    res.status(200).json(allResources)
+})
+
+router.post('/', async (req, res) => {   ///  fix this end point 
+    const something = await rm.insert(req.body)
+    res.status(201).json(something)
+
 })
 
 module.exports = router
