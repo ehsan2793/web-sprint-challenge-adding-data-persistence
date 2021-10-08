e316ed0e59c32a221abf87f79f7a95ff0dbbd17c
ehsan2793 <ehsan2793@gmail.com>
2021-10-07 23:19:34 -0700

    now we can see all projects

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,27 @@
-// const db = require('../../data/dbConfig')
+const db = require('../../data/dbConfig')
+
+const getAll = async () => {
+    const allprojects = await db('projects')
+        .select('project_name', 'project_description', 'project_completed')
+    return allprojects
+}
+
+const getById = async (id) => {
+    const project = await db('projects')
+        .where({ project_id: id }).first();
+    return project;
+};
+
+const insert = async (project) => {
+    const [id] = await db('projects').insert(project)
+    return getById(id)
+}
+
+
+
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+}
